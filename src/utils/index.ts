d7b6c635706bc27f640f94c18010d0ef10df5e70
jacobg1 <greenwald.j8@gmail.com>
2025-06-06 21:03:14 -0400

    add method to capitalize first letter of word

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,3 +24,7 @@ export const includesTags = (
 ): boolean => {
   return normalizePath.includes("tags") && destination.includes("blog");
 };
+
+export function capitalizeWord(word: string): string {
+  return word[0].toUpperCase() + word.slice(1).toLocaleLowerCase();
+}
