b9d876c40b5b1af1f93fe4e71cdc6bd230436846
jacobg1 <greenwald.j8@gmail.com>
2025-06-12 15:22:48 -0400

    move styles into component

--- src/components/header/header-emoji.tsx
+++ src/components/header/header-emoji.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import type { SxProps } from "@mui/material";
 import Box from "@mui/material/Box";
 
 import { RegularEmoji, WackyEmoji } from "../global/emoji";
@@ -15,16 +16,15 @@ const emojis = [
     name: "sunglasses-face",
     isWacky: true,
   },
-  {
-    emoji: "🍩",
-    name: "doughnut",
-    isWacky: true,
-  },
 ];
+
+const headerEmojiStyles: SxProps = {
+  fontSize: { xs: "20px", sm: "17px", md: "22px" },
+};
 
 export function HeaderEmoji(): JSX.Element {
   return (
-    <Box component="span" className="header-emojis">
+    <Box component="span" sx={headerEmojiStyles}>
       {emojis.map(({ emoji, name, isWacky }) => {
         const EmojiComponent = isWacky ? WackyEmoji : RegularEmoji;
         return (
