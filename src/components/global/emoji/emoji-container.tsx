b5257dc6f2c413b8a4b5048d682d7412dd7804bd
jacobg1 <greenwald.j8@gmail.com>
2025-06-12 21:10:17 -0400

    add mui component

--- src/components/global/emoji/emoji-container.tsx
+++ src/components/global/emoji/emoji-container.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+
 import { EmojiProps } from "../../../types";
 
 export function EmojiContainer({ name, children }: EmojiProps): JSX.Element {
   return (
-    <span className={`emoji ${name}`} role="img" aria-label={name}>
+    <Box
+      component="span"
+      className={`emoji ${name}`}
+      role="img"
+      aria-label={name}
+    >
       {children}
-    </span>
+    </Box>
   );
 }
