0c167f9091d9bc1e72b379a0a6a9d3cd2843a014
jacobg1 <greenwald.j8@gmail.com>
2025-06-30 09:15:53 -0400

    implement custom hook

--- src/wrappers/wrap-root-element.tsx
+++ src/wrappers/wrap-root-element.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo, ReactElement, useSyncExternalStore } from "react";
+import React, { useMemo, type ReactElement } from "react";
 
 import { ThemeProvider } from "@emotion/react";
 import Box from "@mui/material/Box";
 import { GatsbyBrowser } from "gatsby";
 
 import { SiteThemeContext } from "../context/site-theme-context";
+import { useLocalStorage } from "../hooks/use-local-storage";
 import { getTheme } from "../theme";
 import { SITE_THEME_KEY, SiteTheme } from "../types/enum";
 
@@ -12,37 +13,15 @@ interface WrapperProps {
   element: ReactElement;
 }
 
-function themeFromLocalStorage(): SiteTheme {
-  return (
-    (localStorage.getItem(SITE_THEME_KEY) as SiteTheme) || SiteTheme.SILVER
-  );
-}
-
-function themeFromServer(): null {
-  return null;
-}
-
-function subscribeToLocalStorage(cb: () => void): () => void {
-  window.addEventListener("storage", cb);
-  return () => {
-    window.removeEventListener("storage", cb);
-  };
-}
-
-function setSiteTheme(selectedTheme: SiteTheme): void {
-  localStorage.setItem(SITE_THEME_KEY, selectedTheme);
-  window.dispatchEvent(new Event("storage"));
-}
-
 const Wrapper = ({ element }: WrapperProps): JSX.Element | null => {
-  const selectedTheme = useSyncExternalStore(
-    subscribeToLocalStorage,
-    themeFromLocalStorage,
-    themeFromServer
+  const [selectedTheme, setSiteTheme] = useLocalStorage(
+    SITE_THEME_KEY,
+    SiteTheme.SILVER,
+    ""
   );
 
   const theme = useMemo(() => {
-    if (selectedTheme) return getTheme(selectedTheme);
+    if (selectedTheme) return getTheme(selectedTheme as SiteTheme);
     return null;
   }, [selectedTheme]);
 
