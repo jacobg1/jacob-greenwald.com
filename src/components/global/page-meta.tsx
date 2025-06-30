7c8fa44961df15dc150f6e463a35915b6e9e8b33
jacobg1 <greenwald.j8@gmail.com>
2025-06-29 20:06:52 -0400

    remove react-helmet

--- src/components/global/page-meta.tsx
+++ src/components/global/page-meta.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 
-import { Helmet } from "react-helmet";
-
 import { useMetadataQuery } from "../../hooks/use-metadata-query";
-import { PageMetaProps, SiteMetadata } from "../../types";
+import { SiteMetadata } from "../../types";
+
+export interface PageMetaProps {
+  metaTitle?: string;
+  metaDescription?: string;
+  slug?: string;
+}
 
 export const PageMeta = ({
   metaDescription,
@@ -15,55 +19,64 @@ export const PageMeta = ({
 
   const seoTitle = metaTitle || title;
   const seoDescription = metaDescription || description;
+  const lang = "en";
+
+  const metaLinks = [
+    {
+      rel: "canonical",
+      href: slug ? `${siteUrl}${slug}` : siteUrl,
+    },
+  ];
+
+  const metaTags = [
+    {
+      name: "description",
+      content: seoDescription,
+    },
+    {
+      name: "keywords",
+      content: keywords.join(),
+    },
+    {
+      name: "meta",
+      content: "initial-scale=1, width=device-width",
+    },
+    {
+      name: "og:type",
+      content: "website",
+    },
+    {
+      name: "twitter:card",
+      content: "summary",
+    },
+    {
+      name: "og:title",
+      content: seoTitle,
+    },
+    {
+      name: "og:description",
+      content: seoDescription,
+    },
+    {
+      name: "twitter:title",
+      content: seoTitle,
+    },
+    {
+      name: "twitter:description",
+      content: seoDescription,
+    },
+  ];
 
   return (
-    <Helmet
-      title={seoTitle}
-      htmlAttributes={{ lang: "en" }}
-      meta={[
-        {
-          name: "description",
-          content: seoDescription,
-        },
-        {
-          name: "keywords",
-          content: keywords.join(),
-        },
-        {
-          name: "meta",
-          content: "initial-scale=1, width=device-width",
-        },
-        {
-          name: "og:type",
-          content: "website",
-        },
-        {
-          name: "twitter:card",
-          content: "summary",
-        },
-        {
-          name: "og:title",
-          content: seoTitle,
-        },
-        {
-          name: "og:description",
-          content: seoDescription,
-        },
-        {
-          name: "twitter:title",
-          content: seoTitle,
-        },
-        {
-          name: "twitter:description",
-          content: seoDescription,
-        },
-      ]}
-      link={[
-        {
-          rel: "canonical",
-          href: slug ? `${siteUrl}${slug}` : siteUrl,
-        },
-      ]}
-    />
+    <>
+      <html lang={lang} />
+      <title>{seoTitle}</title>
+      {metaLinks.map(({ rel, href }) => (
+        <link key={rel} id={rel} rel={rel} href={href} />
+      ))}
+      {metaTags.map(({ name, content }) => (
+        <meta key={name} id={name} name={name} content={content} />
+      ))}
+    </>
   );
 };
