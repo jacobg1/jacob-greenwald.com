0a5480243d98f5689db021e4588fc3bb66816c2a
jacobg1 <greenwald.j8@gmail.com>
2025-10-07 18:46:26 -0400

    add mock projects

--- __utils__/mock-data.ts
+++ __utils__/mock-data.ts
@@ -1,4 +1,10 @@
-import { type SiteMetadata, type Skill, SkillsEnum } from "../src/types";
+import {
+  ProjectIconName,
+  type ProjectsListContent,
+  type SiteMetadata,
+  type Skill,
+  SkillsEnum,
+} from "../src/types";
 
 export const mockSkills: Skill[] = [
   { text: "test skill", type: SkillsEnum.BACKEND, website: "test-site" },
@@ -10,5 +16,44 @@ export const mockMetadata: SiteMetadata = {
   email: "test@email.com",
   description: "test description",
   keywords: ["cool", "keyword!"],
-  siteUrl: "site.site",
+  siteUrl: "site.cool",
+};
+
+export const projectOneContent = "project one content";
+
+export const projectOne = {
+  node: {
+    html: `<p>${projectOneContent}</p>`,
+    id: "project-one-id",
+    frontmatter: {
+      title: "Project One",
+      app: "project-one-app.nice",
+      repo: "roject-one-repo.cool",
+      order: 1,
+      iconName: ProjectIconName.GRAPH,
+    },
+  },
+};
+
+export const projectTwoContent = "project two content";
+
+export const projectTwo = {
+  node: {
+    html: `<p>${projectTwoContent}</p>`,
+    id: "project-two-id",
+    frontmatter: {
+      title: "Project Two",
+      app: "project-two-app.nice",
+      repo: "project-two-repo.cool",
+      order: 2,
+      iconName: ProjectIconName.VINYL,
+    },
+  },
+};
+
+export const mockProjects: ProjectsListContent = {
+  projects: {
+    edges: [projectOne, projectTwo],
+    pageInfo: { itemCount: 2 },
+  },
 };
