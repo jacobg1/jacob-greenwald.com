c088e17c20f4ecbdf182025d0e3ceadf9b2196ba
jacobg1 <greenwald.j8@gmail.com>
2025-10-08 14:59:34 -0400

    update test utils

--- __utils__/general-utils.ts
+++ __utils__/general-utils.ts
@@ -1,6 +1,6 @@
-import type { HtmlString } from "../src/types";
+import type { HtmlString, ProjectNode, TextMatcher } from "../src/types";
 
-export function parseHtmlString(htmlString: HtmlString): string {
+function parseHtmlString(htmlString: HtmlString): string {
   const parser = new DOMParser();
   const parsedDoc = parser.parseFromString(htmlString, "text/html");
 
@@ -9,4 +9,36 @@ export function parseHtmlString(htmlString: HtmlString): string {
   if (!textContent) return "";
 
   return textContent.trim();
+}
+
+export function updateProjectOrder(
+  order: number,
+  project: ProjectNode
+): ProjectNode {
+  return {
+    node: {
+      ...project.node,
+      frontmatter: {
+        ...project.node.frontmatter,
+        order,
+      },
+    },
+  };
+}
+
+export function testProject(
+  getByText: TextMatcher,
+  getByTestId: TextMatcher,
+  {
+    node: {
+      html,
+      frontmatter: { title, iconName, app, repo },
+    },
+  }: ProjectNode
+): void {
+  expect(getByText(title)).toBeVisible();
+  expect(getByText(parseHtmlString(html))).toBeVisible();
+  expect(getByTestId(`${iconName}-icon`)).toBeVisible();
+  expect(getByTestId(`${title}-app-button`)).toHaveAttribute("href", app);
+  expect(getByTestId(`${title}-repo-button`)).toHaveAttribute("href", repo);
 }
