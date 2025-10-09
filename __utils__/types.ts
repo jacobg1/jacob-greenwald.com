38325659f6eb051c6e72bab78d03c987e3056fc6
jacobg1 <greenwald.j8@gmail.com>
2025-10-09 16:46:13 -0400

    update types

--- __utils__/types.ts
+++ __utils__/types.ts
@@ -1,6 +1,8 @@
 import type { Matcher, SelectorMatcherOptions } from "@testing-library/react";
 import type { PageProps } from "gatsby";
 
+import type { SingleBlogProps } from "../src/types";
+
 export type TextMatcher = (
   id: Matcher,
   options?: SelectorMatcherOptions
@@ -17,3 +19,17 @@ export type GetMockPageProps<T, C, S> = Omit<
   PageProps<T, C, unknown, S>,
   "data"
 >;
+
+export type PostNumWord = {
+  [x: string]: {
+    reg: string;
+    up: string;
+  };
+};
+
+export interface NextOrPrev {
+  nextBlog: boolean;
+  prevBlog: boolean;
+}
+
+export type NextOrPrevBlogCreate = Pick<SingleBlogProps, "next" | "previous">;
