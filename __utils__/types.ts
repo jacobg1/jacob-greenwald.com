f31f1e70261904ff8b84ba0321d5078ec4a0ecc2
jacobg1 <greenwald.j8@gmail.com>
2025-10-09 10:11:20 -0400

    update types

--- __utils__/types.ts
+++ __utils__/types.ts
@@ -1,4 +1,5 @@
 import type { Matcher, SelectorMatcherOptions } from "@testing-library/react";
+import type { PageProps } from "gatsby";
 
 export type TextMatcher = (
   id: Matcher,
@@ -9,3 +10,10 @@ export interface MockMetadata {
   title: string;
   canonicalHref: string;
 }
+
+export type MockParams = { test: string };
+
+export type GetMockPageProps<T, C, S> = Omit<
+  PageProps<T, C, unknown, S>,
+  "data"
+>;
