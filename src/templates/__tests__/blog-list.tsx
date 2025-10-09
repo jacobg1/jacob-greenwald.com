6514e5b089933a19ca1d3f5bb1e104ea14b778e1
jacobg1 <greenwald.j8@gmail.com>
2025-10-09 19:50:36 -0400

    test blog list pagination

--- src/templates/__tests__/blog-list.tsx
+++ src/templates/__tests__/blog-list.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import * as Gatsby from "gatsby";
 
 import {
@@ -14,6 +14,7 @@ import type { BlogsListProps, BlogListContext } from "../../types";
 import BlogListPage, { Head } from "../blog-list";
 
 const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
+const navigate = jest.spyOn(Gatsby, "navigate");
 
 const title = "Blog Posts";
 
@@ -82,6 +83,38 @@ describe("blog list template", () => {
 
     expect(getByTestId("NavigateBeforeIcon").parentElement).toBeDisabled();
     expect(getByTestId("NavigateNextIcon").parentElement).toBeEnabled();
+  });
+
+  it("can change pages", () => {
+    const { getByLabelText } = render(
+      <BlogListPage data={data} {...mockPageProps} />
+    );
+
+    [1, 2].forEach((num) => {
+      const exp = new RegExp(`page ${num}`);
+      fireEvent.click(getByLabelText(exp));
+
+      const href = num === 1 ? "/blog" : `/blog/${num}`;
+      expect(navigate).toHaveBeenNthCalledWith(num, href);
+    });
+  });
+
+  it("pagination doesn't show if only one page of posts", () => {
+    const pageContext = { totalPages: 1, currentPage: 1 };
+    const pageProps = getMockPageProps<BlogsListProps, BlogListContext>(
+      mockBlogList,
+      pageContext
+    );
+    const pageData = {
+      ...mockBlogList,
+      pageContext,
+    };
+    const { queryByLabelText } = render(
+      <BlogListPage data={pageData} {...pageProps} />
+    );
+
+    const exp = new RegExp(`page 1`);
+    expect(queryByLabelText(exp)).toBeNull();
   });
 
   it("renders metadata properly", () => {
