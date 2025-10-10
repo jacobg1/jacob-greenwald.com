90a71b93b46a57ef2fdf6f19a12db6466c893941
jacobg1 <greenwald.j8@gmail.com>
2025-10-10 17:08:09 -0400

    add more test coverage

---
+++ src/components/blog/__tests__/post-links.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+import { render } from "@testing-library/react";
+
+import { postOne, postTwo } from "../../../../__utils__";
+import { NextPostLink, PreviousPostLink } from "../post-links";
+
+describe("post links", () => {
+  it("renders the next post link", () => {
+    const { container } = render(<NextPostLink post={postOne} />);
+    expect(container).not.toBeEmptyDOMElement();
+  });
+
+  it("the next post link is null if no next post is available", () => {
+    const { container } = render(<NextPostLink post={null} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("renders the previous post link", () => {
+    const { queryByText } = render(<PreviousPostLink post={postTwo} />);
+    expect(queryByText("prev")).not.toBeNull();
+  });
+
+  it("the prev post link is null if no prev post is available", () => {
+    const { queryByText } = render(<PreviousPostLink post={null} />);
+    expect(queryByText("prev")).toBeNull();
+  });
+});
