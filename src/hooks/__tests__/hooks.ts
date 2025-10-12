2dd5d4888676a1ed77b3b7524dc24d28d70263c8
jacobg1 <greenwald.j8@gmail.com>
2025-10-12 14:26:55 -0400

    finish test coverage of hooks

--- src/hooks/__tests__/hooks.ts
+++ src/hooks/__tests__/hooks.ts
@@ -1,10 +1,13 @@
 import { act, renderHook } from "@testing-library/react";
+import * as Gatsby from "gatsby";
 
-import { setupLocalStore } from "../../../__utils__";
-import { SiteTheme } from "../../types";
+import { mockNavLinks, setupLocalStore } from "../../../__utils__";
+import { type NavLinkItem, SiteTheme } from "../../types";
 import { useLocalStorage } from "../use-local-storage";
+import { useNavLinksQuery } from "../use-navlinks-query";
 
 const testKey = "test-key";
+const mockUseStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
 
 describe("hooks", () => {
   afterEach(() => {
@@ -50,5 +53,18 @@ describe("hooks", () => {
     expect(result.current[0]).toBe(SiteTheme.SILVER);
     act(() => result.current[1](SiteTheme.CLASSIC));
     expect(result.current[0]).toBe(SiteTheme.SILVER);
+  });
+
+  it("useNavLinksQuery returns proper the data", () => {
+    const input = {
+      markdownRemark: {
+        frontmatter: { navLinks: mockNavLinks },
+      },
+    };
+
+    mockUseStaticQuery.mockImplementationOnce(() => input);
+    const { result } = renderHook(() => useNavLinksQuery<NavLinkItem[]>());
+
+    expect(result.current).toEqual(mockNavLinks);
   });
 });
